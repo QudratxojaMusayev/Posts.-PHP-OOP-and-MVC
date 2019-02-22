6ba53c3bc1d90a5f499987f353b1d5143dc89301
Qudratxoja.Musayev <qudratxoja.musayev@gmail.com>
2019-02-22 10:48:22 +0500

    Core class has been finished. Default controller created

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -5,12 +5,30 @@
         protected $params = [];
 
         public function __construct() {
-            $this->getURL();
+            $url = $this->getURL();
+
+            if (file_exists("../app/controllers/" . ucwords($url[0]) . ".php")) {
+                // If exists set as current controller
+                $this->currentController = ucwords($url[0]);
+                // Unset 0 index
+                unset($url[0]);
+            }
+
+            // Require the controller
+            require_once "../app/controllers/". $this->currentController .".php";
+
+            // Instantiate controller class
+            $this->curretController = new $this->currentController; // $pages = new Pages;
         }
 
         public function getURL()
         {
-            echo $_GET["url"];
+            if (isset($_GET["url"])) {
+                $url = rtrim($_GET["url"], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode("/", $url);
+                return $url;
+            }
         }
     }
 ?>
