e37dd39903444cdd1d7f6a102b2b5bf8bda0311d
Qudratxoja.Musayev <qudratxoja.musayev@gmail.com>
2019-03-01 09:51:22 +0500

    Config file created and finished. Auto load libraries added

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,10 @@
 <?php 
-    // Load libraries
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Database.php';
+    // Load config file
+    require_once "config/config.php";
+    
+    // Auto load libraries
+    spl_autoload_register(function ($className)
+    {
+       require_once "libraries/". $className .".php";
+    });
 ?>
---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+    // App folder root
+    define("APPROOT", dirname(dirname(__FILE__)));
+    
+    // URL root
+    define("URLROOT", "http://posts.com/public");
+
+    // Site name
+    define("SITENAME", "SharePosts");
+?>
